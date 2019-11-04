589297bf1784fda7f8c868190a9201c6eb8c1a48
Erik Nilsen <erikni@ostfoldfk.no>
2019-11-04 10:35:16 +0100

    Cleaned the main file

---
+++ src/functions/updateStats.js
@@ -0,0 +1,70 @@
+const phin = require('phin');
+const GBLAPIError = require('./GBLAPIError');
+
+module.exports = async function (serverCount, shardCount, id, auth) {
+    return phin({
+        method: "POST",
+        url: `https://glennbotlist.xyz/api/stats/bot/${id}`,
+        data: {
+            serverCount,
+            shardCount,
+            auth
+        },
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        parse: "json"
+    }).then((p) => {
+        if (p.statusCode !== 200) switch (p.statusCode) {
+            case 400:
+                throw new GBLAPIError({
+                    statusCode: p.statusCode,
+                    body: p.body,
+                    type: "Bad Request"
+                });
+
+            case 401:
+                throw new GBLAPIError({
+                    statusCode: p.statusCode,
+                    body: p.body,
+                    type: "Unauthorized"
+                });
+
+            case 403:
+                throw new GBLAPIError({
+                    statusCode: p.statusCode,
+                    body: p.body,
+                    type: "Bad Request"
+                });
+
+            case 404:
+                throw new GBLAPIError({
+                    statusCode: p.statusCode,
+                    body: p.body,
+                    type: "Not Found"
+                });
+
+            case 500:
+            case 502:
+                throw new GBLAPIError({
+                    statusCode: p.statusCode,
+                    body: p.body,
+                    type: "Server Error"
+                });
+
+            default:
+                throw new GBLAPIError({
+                    statusCode: p.statusCode,
+                    body: p.body,
+                    type: "Unkown"
+                });
+        }
+        return {
+            message: p.body.message || "Successful request.",
+            success: p.statusCode === 200
+        };
+    }).catch(err => {
+        throw err;
+    });
+
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const EventEmitter = require('eventemitter3');
-const phin = require('phin');
-const GBLAPIError = require('./GBLAPIError');
 const util = require('util');
-
+const getBot = require('./functions/getBot');
+const getUser = require('./functions/getUser');
+const updateStats = require('./functions/updateStats');
+const hasVoted = require('./functions/hasVoted');
 class GBLAPI extends EventEmitter {
     /**
      * Main Class
@@ -53,79 +54,7 @@ class GBLAPI extends EventEmitter {
      */
     async getBot(id = this.id) {
         if (!id) throw new TypeError("Missing Bot ID");
-        return phin({
-            url: `https://glennbotlist.xyz/api/bot/${id}`,
-            parse: "json"
-        }).then((b) => {
-            if (b.statusCode !== 200) switch (b.statusCode) {
-                case 400:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Bad Request"
-                    });
-
-                case 401:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Unauthorized"
-                    });
-
-                case 403:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Bad Request"
-                    });
-
-                case 404:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Not Found"
-                    });
-
-                case 500:
-                case 502:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Server Error"
-                    });
-
-                default:
-                    throw new GBLAPIError({
-                        statusCode: b.statusCode,
-                        body: b.body,
-                        type: "Unkown"
-                    });
-            }
-            return {
-                id: b.body.id || id,
-                name: b.body.name || "Unknown",
-                owner: b.body.owner,
-                owners: b.body.owners,
-                library: b.body.library,
-                monthly_upvotes: b.body.monthlyUpvotes || 0,
-                all_time_upvotes: b.body.allTimeUpvotes || 0,
-                website: b.body.website || "None",
-                github: b.body.githubUrl || "None",
-                short_desc: b.body.shortDesc,
-                support_server: b.body.supportServerInvite || "",
-                prefix: b.body.prefix,
-                verified: b.body.verified || false,
-                trusted: b.body.trusted || false,
-                vanity_url: b.body.vanityUrl || "",
-                featured: b.body.featured || false,
-                invite: b.body.inviteUrl || `https://discordapp.com/oauth2/authorize?client_id=${b.body.id || id}&scope=bot`,
-                server_count: b.body.serverCount || 0,
-                shard_count: b.body.shardCount || 0,
-                tags: b.body.tags,
-                votes: b.body.votes,
-                rates: b.body.rates
-            }
-        }).catch(err => { throw err; });
+        getBot(id);
     }
 
     /**
@@ -135,68 +64,7 @@ class GBLAPI extends EventEmitter {
      */
     async getUser(id = this.id) {
         if (!id) throw new TypeError("Missing User ID");
-        return phin({
-            url: `https://glennbotlist.xyz/api/profiles/${id}`,
-            parse: "json"
-        }).then((b) => {
-            if (b.statusCode !== 200) switch (b.statusCode) {
-                case 400:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Bad Request"
-                    });
-
-                case 401:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Unauthorized"
-                    });
-
-                case 403:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Bad Request"
-                    });
-
-                case 404:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Not Found"
-                    });
-
-                case 500:
-                case 502:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Server Error"
-                    });
-
-                default:
-                    throw new GBLAPIError({
-                        statusCode: b.statusCode,
-                        body: b.body,
-                        type: "Unkown"
-                    });
-            }
-            return {
-                id: b.body.id || id,
-                username: b.body.username || "Unknown",
-                discriminator: b.body.discriminator || "0000",
-                avatar: b.body.avatar,
-                background: b.body.background || null,
-                bio: b.body.bio,
-                isVerified: b.body.verified || false,
-                isMod: b.body.mod || false,
-                isAdmin: b.body.admin || false,
-                karma: b.body.karma || 0,
-                totalKarma: b.body.totalKarma || 0,
-            }
-        }).catch(err => { throw err; });
+        getUser(id);
     }
 
     /**
@@ -284,71 +152,7 @@ class GBLAPI extends EventEmitter {
         if (this._logging === true) {
             console.log(`[GlennBotList] Posting Stats...`);
         }
-        let authorization = auth;
-        return phin({
-            method: "POST",
-            url: `https://glennbotlist.xyz/api/stats/bot/${id}`,
-            data: {
-                serverCount,
-                shardCount,
-                authorization
-            },
-            headers: {
-                "Content-Type": 'application/json'
-            },
-            parse: "json"
-        }).then((p) => {
-            if (p.statusCode !== 200) switch (p.statusCode) {
-                case 400:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Bad Request"
-                    });
-
-                case 401:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Unauthorized"
-                    });
-
-                case 403:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Bad Request"
-                    });
-
-                case 404:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Not Found"
-                    });
-
-                case 500:
-                case 502:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Server Error"
-                    });
-
-                default:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Unkown"
-                    });
-            }
-            return {
-                message: p.body.message || "Successful request.",
-                success: p.statusCode === 200
-            };
-        }).catch(err => {
-            throw err;
-        });
+        updateStats(serverCount, shardCount, id, auth);
     }
 
     /**
@@ -368,59 +172,9 @@ class GBLAPI extends EventEmitter {
         if (!id) {
             if (!this._id) throw new TypeError("Missing Bot ID");
         }
-        return phin({
-            url: `https://glennbotlist.xyz/api/bot/${id}`,
-            parse: "json"
-        }).then((b) => {
-            if (b.statusCode !== 200) switch (b.statusCode) {
-                case 400:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Bad Request"
-                    });
-                case 401:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Unauthorized"
-                    });
-                case 403:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Bad Request"
-                    });
-
-                case 404:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Not Found"
-                    });
-
-                case 500:
-                case 502:
-                    throw new GBLAPIError({
-                        statusCode: p.statusCode,
-                        body: p.body,
-                        type: "Server Error"
-                    });
-
-                default:
-                    throw new GBLAPIError({
-                        statusCode: b.statusCode,
-                        body: b.body,
-                        type: "Unkown"
-                    });
-            }
-            let voted = b.body.votes.map(i => i.id == uid);
-            if (voted) { return true } else { return false }
-        }).catch(err => { throw err; });
+        hasVoted(uid, id)
     }
 }
-
-GBLAPI.prototype.getBotVotes = util.deprecate(GBLAPI.prototype.getBotVotes, 'GBL#getBotVotes: use GBL#hasVoted instead');
 
 module.exports = GBLAPI;
 
