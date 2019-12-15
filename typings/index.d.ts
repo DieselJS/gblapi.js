69fdbf1a3a29c7f338025ddf6a90008afe2b9860
DieselGaming <44411085+DieselGaming@users.noreply.github.com>
2019-12-15 18:01:25 +0000

    Typings

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,13 +1,1 @@
 declare module 'gblapi.js';
-export = GBLAPI;
-import EventEmitter = require('eventemitter3');
-
-declare class GBLAPI extends EventEmitter {
-    constructor(id: string, token: string, options: object);
-    constructor(id: string, token: string);
-
-    public updateStats(serverCount: number, shardCount: number): Promise<object>
-
-    public token?: string;
-    public id?: string;
-}
